f851f531d48c31ff05ac2faa7edb354fe44d4ee8
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2018-09-03 08:56:24 -0300

    Ugandan Somalian Global elite 2

--- easteregg.js
+++ easteregg.js
@@ -1,8 +1,26 @@
-var cont = 1;
-function easteregg()
+var cont; var ind; var easteregg;
+function onLoad()
 {
-	if (cont == 5)
-		alert('Hello am 48 year man from somalia. Sorry for my bed england. I selled my wife for internet connection for play "conter strik" and i want to become the goodest player like you I play with 400 ping on brazil and i am global elite 2. pls no copy pasterio my story');
-	else
-		cont++;
+	cont = 0;
+	ind = 0;
+	easteregg = new Array("EasterEgg","( ͡° ͜ʖ ͡°)","Você deseja continuar?","Você quer descobrir o que está por trás da vida, da verdade e do universo",
+		"Você quer mesmo saber?","Eu avisei","3","2","1","Eu avisei","Hello am 48 year man from somalia. Sorry for my bed england. I selled my wife for internet connection for play \"conter strik\" and i want to become the goodest player like you I play with 400 ping on brazil and i am global elite 2. pls no copy pasterio my story");
 }
+function logoClick()
+{
+	cont++;
+	if (cont == 5)
+	{
+		alert(easteregg[ind]);
+		cont = 0;
+		ind++;
+	}
+}
+
+
+
+
+
+
+
+
