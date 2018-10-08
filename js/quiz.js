eaca7041bd73368a46fe7cd01f00c08985b89358
GugaH13 <cc18180@g.unicamp.br>
2018-10-08 11:57:40 -0300

    eaeae
    aeaeae

--- js/quiz.js
+++ js/quiz.js
@@ -137,3 +137,18 @@ function botaoRespostas()
   janela.document.write(texto);
   janela.focus();
 }
+
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('btnLogin');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeBtn(){
+  modal.style.display = 'none';
+}
