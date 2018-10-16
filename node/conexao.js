bceb06e201984089f72a33821082abfb72b0bb3d
pedrogmo <cc18174@g.unicamp.br>
2018-10-16 09:11:17 -0300

    conexão funcionando

--- node/conexao.js
+++ node/conexao.js
@@ -1,14 +1,29 @@
-capp.use(bodyParser.json()); //acrescentando informacoes de cabecalho para suportar o CORS 
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const porta = 3000; //porta padrão 
+const sql = require('mssql'); 
+const conexaoStr = "Server=regulus.cotuca.unicamp.br;Database=PR118174;User Id=PR118174;Password=PR118174;";
+
+//conexao com BD 
+sql.connect(conexaoStr) 
+.then(conexao => global.conexao = conexao) 
+.catch(erro => console.log(erro)); 
+
+// configurando o body parser para pegar POSTS mais tarde 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json()); //acrescentando informacoes de cabecalho para suportar o CORS 
 app.use(function(req, res, next) { 
 	res.header("Access-Control-Allow-Origin", "*"); 
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
 	res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PATCH, DELETE"); 
 	next(); 
 }); 
+
 //definindo as rotas
 const rota = express.Router(); 
-rota.get('/', (requisicao, resposta) => resposta.json({ mensagem: 'Funcionando!'})); a
-pp.use('/', rota); 
+rota.get('/', (requisicao, resposta) => resposta.json({ mensagem: 'Funcionando!'})); 
+app.use('/', rota); 
 
 //inicia servidor 
 app.listen(porta); 
