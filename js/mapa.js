98ca5679d7eac21430b6c8eb8f30d5b4b715f60f
pedrogmo <cc18174@g.unicamp.br>
2018-10-16 11:11:58 -0300

    as

--- js/mapa.js
+++ js/mapa.js
@@ -37,5 +37,5 @@ for (var i = 0; i < arr.length; i++)
 
 document.getElementById('cbxEstados').onchange = function(){
 	var indice = document.getElementById("cbxEstados").selectedIndex;
-  setTimeout(function(){alert('Você selecionou: ' + estados[indice] + "\nDados:\n" + info[indice]);},100)	
+  setTimeout(function(){alert('Você selecionou: ' + estados[indice] + "\nDados:\n" + info[indice]);},100);
 }
