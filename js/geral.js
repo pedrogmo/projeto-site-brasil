d97bbdba7ec04e3d6be1483dbff33c5e37ec739f
pedrogmo <cc18174@g.unicamp.br>
2018-10-24 07:52:26 -0300

    a

--- js/geral.js
+++ js/geral.js
@@ -122,4 +122,4 @@ document.getElementById("bandeira").onclick = function(){ alert(band); }
 var band = 'A bandeira atual da República Federativa do Brasil foi feita em 1889, durante a República Velha. A frase "Ordem e Progresso"'+
 ' representa o positivismo, ideia segundo a qual a sociedade deve ser científica e autoritária, para que haja desenvolvimento. Acredita-se ' +
 'que as cores verde e amarelo tenham surgido da riqueza presente nas matas e dos metais preciosos (o outro). Contudo, historiadores afirmam que ' +
- 'a escolha dessas cores provém da união entre as famílias reais Bragança (Brasil) e Habsburgo (Áustria).';
+'a escolha dessas cores provém da união entre as famílias reais Bragança (Brasil) e Habsburgo (Áustria).';
