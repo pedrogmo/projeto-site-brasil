6d16f705ec30e6fa0d10719826c6ad65e3c217f1
pedrogmo <cc18174@g.unicamp.br>
2018-10-25 13:45:17 -0300

    Deu tudo certo

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,17 +1,29 @@
 document.getElementById("criarConta").onclick = function(){
-	var email = document.getElementById("email").value;
+	var senha = document.getElementById("senha").value.trim();
+	var senha2 = document.getElementById("confirmarSenha").value.trim();
+	var nome = document.getElementById("nome").value.trim();
+	var pais = document.getElementById("nacionalidade").value.trim();
+	var email = document.getElementById("email").value.trim();
 	var data = document.getElementById("data").value;
 	var dia = parseInt(data.substring(0,2));
 	var mes = parseInt(data.substring(3,5));
-	var ano = parseInt(data.substring(6,10));
-	var senha = document.getElementById("senha").value.trim();
-	var senha2 = document.getElementById("confirmarSenha").value.trim();
-	if (senha == "")
+	var ano = parseInt(data.substring(6,10));	
+	if (senha == "" || senha2 == "")
 	{
 		alert("Senha inválida");
 		document.getElementById("senha").value = document.getElementById("confirmarSenha").value = "";	
 	}
-	if (isNaN(dia) || isNaN(mes) || isNaN(ano) || dia > 31 || mes > 12 || ano > 2018 || ano < 1890 || dia < 1 || mes < 1)
+	else if (nome == "" || /^[a-zA-Z ]+$/.test(nome) == false)
+	{
+		alert("Nome inválido");
+		document.getElementById("nome").value = "";
+	}	
+	else if (pais == "")
+	{
+		alert("País inválido");
+		document.getElementById("nacionalidade").value = "";
+	}
+	else if (isNaN(dia) || isNaN(mes) || isNaN(ano) || dia > 31 || mes > 12 || ano > 2018 || ano < 1890 || dia < 1 || mes < 1)
 	{
 		alert('Data inválida');
 		document.getElementById("data").value = "";
@@ -40,11 +52,11 @@ document.getElementById("criarConta").onclick = function(){
 		{
 			alert('Conta criada');
 			var user = new Object();
-			user.nome = $("#nome").val();
+			user.nome = nome;
 			user.senha = senha;
 			user.email = email;
 			user.aniversario = data;
-			user.pais = $("#nacionalidade").val();
+			user.pais = pais;
 			$.post('http://localhost:3000/usuario', user);
 			location.href = "inicio.html";
 		}
